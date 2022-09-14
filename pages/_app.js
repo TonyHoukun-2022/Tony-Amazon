745ef188a75e4bfb5727d69ccbf625b1c779e32b
Tony Peng <lp1dxing@gmail.com>
2022-09-14 14:28:40 +1000

    fix muiv4 ssr (cannot render updated style after refresh)

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  //method to fix muiv4 ssr (cannot render updated style after refresh)
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
   return <Component {...pageProps} />
 }
 
